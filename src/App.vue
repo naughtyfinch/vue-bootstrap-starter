2134f1f85860243cc04efead058051cd70cacd7c
naughtyfinch <156558342+naughtyfinch@users.noreply.github.com>
2024-03-08 15:43:07 +0530

    feat: starter vue template with bootstrap and vue router

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <RouterLink to="/" class="nav-item">Home</RouterLink>
+          <RouterLink to="/about" class="nav-item">About</RouterLink>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
